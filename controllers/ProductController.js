1d3c2e9d565653628a17c7a4a88f84359c2b7efb
AkihikoIzuru <winandaalvino0@gmail.com>
2025-04-09 22:23:17 +0700

    Commit - fix cloudinary import double declare

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,7 +1,6 @@
 const Product = require("../models/Product");
 const cloudinary = require("../config/cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("cloudinary").v2;
 
 exports.getProducts = async (req, res) => {
   try {
